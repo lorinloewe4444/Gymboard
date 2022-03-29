6ca2431ce95e307951b167fd5f3c219467de5b9e
macello <96392254+macello@users.noreply.github.com>
2022-03-29 22:37:51 +0200

    Add files via upload

--- Database.py
+++ Database.py
@@ -4,39 +4,40 @@ import sqlite3
 con = sqlite3.connect('GymBoard.db')
 cur = con.cursor()
 
-sql = "CREATE TABLE comments(" \
+sql = "CREATE TABLE comment(" \
       "id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, " \
-      "id_Comments INTEGER NOT NULL UNIQUE, " \
-      "id_Documents INTEGER NOT NULL UNIQUE, " \
-      "id_User INTEGER NOT NULL UNIQUE, " \
+      "id_Comment INTEGER, " \
+      "id_Document INTEGER NOT NULL, " \
+      "id_User INTEGER NOT NULL, " \
       "comment TEXT NOT NULL, " \
       "datum TEXT NOT NULL)"
 cur.execute(sql)
 
 sql = "CREATE TABLE document(" \
       "id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, " \
-      "id_user INTEGER NOT NULL UNIQUE, " \
+      "id_User INTEGER NOT NULL, " \
+      "name TEXT NOT NULL, " \
       "path TEXT NOT NULL UNIQUE, " \
       "datum TEXT NOT NULL)"
 cur.execute(sql)
 
-sql = "CREATE TABLE likes(" \
+sql = "CREATE TABLE like(" \
       "id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, " \
-      "id_User INTEGER NOT NULL UNIQUE, " \
-      "id_Document INTEGER NOT NULL UNIQUE, " \
-      "id_Comment INTEGER UNIQUE)"
+      "id_User INTEGER NOT NULL, " \
+      "id_Document INTEGER, " \
+      "id_Comment INTEGER)"
 cur.execute(sql)
 
-sql = "CREATE TABLE tags(" \
+sql = "CREATE TABLE tag(" \
       "id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, " \
       "name TEXT NOT NULL UNIQUE, " \
       "level INTEGER)"
 cur.execute(sql)
 
-sql = "CREATE TABLE tags_Document(" \
+sql = "CREATE TABLE tag_Document(" \
       "id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, " \
-      "id_Tags INTEGER NOT NULL UNIQUE, " \
-      "id_Document INTEGER NOT NULL UNIQUE)"
+      "id_Tag INTEGER NOT NULL, " \
+      "id_Document INTEGER NOT NULL)"
 cur.execute(sql)
 
 sql = "CREATE TABLE user(" \
---
+++ testData.py
@@ -0,0 +1,111 @@
+import sqlite3
+
+connection = sqlite3.connect('GymBoard.db')
+cursor = connection.cursor()
+
+# -- user
+sql = "INSERT INTO user(eMail, nickName, status, role, datum) VALUES('asterix.gallien@stud.gyminterlaken.ch', 'asterix', 0, 0, '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO user(eMail, nickName, status, role, datum) VALUES('idefix.gallien@stud.gyminterlaken.ch', 'idefix', 1, 1, '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO user(eMail, nickName, status, role, datum) VALUES('obelix.gallien@stud.gyminterlaken.ch', 'obelix', 1, 1, '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+# -- document
+sql = "INSERT INTO document(id_User, name, path, datum) VALUES(1, 'firstDocument', 'path1', '22:03:30')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO document(id_User, name, path, datum) VALUES(3, 'secondDocument', 'path2', '22:03:30')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO document(id_User, name, path, datum) VALUES(1, 'thirdDocument', 'path3', '22:03:30')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO document(id_User, name, path, datum) VALUES(2, 'fourthDocument', 'path4', '22:03:30')"
+cursor.execute(sql)
+connection.commit()
+
+# -- tags
+sql = "INSERT INTO tag(name, level) VALUES('gym1', 0)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag(name, level) VALUES('gym2', 0)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag(name, level) VALUES('Chemie', 1)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag(name, level) VALUES('Juchler', 2)"
+cursor.execute(sql)
+connection.commit()
+
+# -- tags_Document
+sql = "INSERT INTO tag_Document(id_Document, id_Tag) VALUES(1, 1)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag_Document(id_Document, id_Tag) VALUES(1, 3)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag_Document(id_Document, id_Tag) VALUES(1, 4)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag_Document(id_Document, id_Tag) VALUES(2, 2)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag_Document(id_Document, id_Tag) VALUES(3, 2)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO tag_Document(id_Document, id_Tag) VALUES(4, 1)"
+cursor.execute(sql)
+connection.commit()
+
+# -- comments
+sql = "INSERT INTO comment(id_Document, id_User, comment, datum) VALUES(1, 3, 'Ein völlig sinnloser Kommentar', '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO comment(id_Document, id_User, comment, datum) VALUES(2, 1, 'Ein völlig sinnvoller Kommentar', '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO comment(id_Document, id_User, comment, datum) VALUES(3, 2, 'Ein weiterer Kommentar', '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO comment(id_Document, id_User, id_Comment, comment, datum) VALUES(2, 3, 2, 'Ein rekursiver Kommentar', '22:03:29')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO like(id_User, id_Document) VALUES(1, 1)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO like(id_User, id_Document) VALUES(2, 1)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO like(id_User, id_Document) VALUES(2, 2)"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO like(id_User, id_Document, id_Comment) VALUES(3, 3, 3)"
+cursor.execute(sql)
+connection.commit()
+
+connection.close()
