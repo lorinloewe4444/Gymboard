ebb90a8a26aa034f5d1a5618eb8b0d885b900636
macello <96392254+macello@users.noreply.github.com>
2022-04-05 15:30:17 +0200

    Update abfrage.py

--- database/abfrage.py
+++ database/abfrage.py
@@ -3,7 +3,7 @@ import sqlite3
 connection = sqlite3.connect('GymBoard.db')
 cursor = connection.cursor()
 
-sql = "SELECT * FROM user"
+sql = "SELECT * FROM users"
 cursor.execute(sql)
 for dsatz in cursor:
     print(dsatz)
