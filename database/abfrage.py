cd0581b583e94c8331456d8529bcc3bc8024b4dd
macello <96392254+macello@users.noreply.github.com>
2022-04-05 16:15:31 +0200

    Update abfrage.py

--- database/abfrage.py
+++ database/abfrage.py
@@ -1,11 +1,45 @@
 import sqlite3
 
-con = sqlite3.connect('GymBoard.db')
-cur = connection.cursor()
 
-sql = "SELECT * FROM users"
-cur.execute(sql)
-for dsatz in cursor:
-    print(dsatz)
 
-con.close()
+def modell():
+    con = sqlite3.connect('GymBoard.db')
+    cur = con.cursor()
+
+    sql = "SELECT * FROM users"
+    cur.execute(sql)
+    for dsatz in cur:
+        print(dsatz)
+
+    con.close()
+
+# Count Likes from a specific Document
+def countLikesDoc(id_Document):
+    likes = 0
+
+    con = sqlite3.connect('GymBoard.db')
+    cur = con.cursor()
+
+    sql = "SELECT id FROM likes where id_Document = " + id_Document
+    cur.execute(sql)
+    for dsatz in cur:
+        likes += 1
+    return likes
+    con.close()
+
+# Count Likes from a specific comment
+def countLikesCom(id_Comment):
+    likes = 0
+
+    con = sqlite3.connect('GymBoard.db')
+    cur = con.cursor()
+
+    sql = "SELECT id FROM likes where id_Document = " + id_Document
+    cur.execute(sql)
+    for dsatz in cur:
+        likes += 1
+    return likes
+    con.close()
+
+
+modell()
