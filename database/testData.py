4c25bd5a7f149db0fe391c469562c263ec021acc
macello <96392254+macello@users.noreply.github.com>
2022-05-03 16:16:34 +0200

    Update testData.py

--- database/testData.py
+++ database/testData.py
@@ -4,32 +4,32 @@ connection = sqlite3.connect('GymBoard.db')
 cursor = connection.cursor()
 
 # -- user
-sql = "INSERT INTO users(eMail, nickName, status, role, datum) VALUES('asterix.gallien@stud.gyminterlaken.ch', 'asterix', 0, 0, '22:03:29')"
+sql = "INSERT INTO users(eMail, nickName, status, role, datum) VALUES('asterix.gallien@stud.gyminterlaken.ch', 'asterix', 0, 0, '1651587175')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO users(eMail, nickName, status, role, datum) VALUES('idefix.gallien@stud.gyminterlaken.ch', 'idefix', 1, 1, '22:03:29')"
+sql = "INSERT INTO users(eMail, nickName, status, role, datum) VALUES('idefix.gallien@stud.gyminterlaken.ch', 'idefix', 1, 1, '1651587175')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO users(eMail, nickName, status, role, datum) VALUES('obelix.gallien@stud.gyminterlaken.ch', 'obelix', 1, 1, '22:03:29')"
+sql = "INSERT INTO users(eMail, nickName, status, role, datum) VALUES('obelix.gallien@stud.gyminterlaken.ch', 'obelix', 1, 1, '1651587175')"
 cursor.execute(sql)
 connection.commit()
 
 # -- document
-sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(1, 'firstDocument', 'path1', '22:03:30')"
+sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(1, 'firstDocument', 'path1', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(3, 'secondDocument', 'path2', '22:03:30')"
+sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(3, 'secondDocument', 'path2', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(1, 'thirdDocument', 'path3', '22:03:30')"
+sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(1, 'thirdDocument', 'path3', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(2, 'fourthDocument', 'path4', '22:03:30')"
+sql = "INSERT INTO documents(id_User, name, path, datum) VALUES(2, 'fourthDocument', 'path4', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
@@ -76,19 +76,19 @@ cursor.execute(sql)
 connection.commit()
 
 # -- comments
-sql = "INSERT INTO comments(id_Document, id_User, comment, datum) VALUES(1, 3, 'Ein völlig sinnloser Kommentar', '22:03:29')"
+sql = "INSERT INTO comments(id_Document, id_User, comment, datum) VALUES(1, 3, 'Ein völlig sinnloser Kommentar', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO comments(id_Document, id_User, comment, datum) VALUES(2, 1, 'Ein völlig sinnvoller Kommentar', '22:03:29')"
+sql = "INSERT INTO comments(id_Document, id_User, comment, datum) VALUES(2, 1, 'Ein völlig sinnvoller Kommentar', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO comments(id_Document, id_User, comment, datum) VALUES(3, 2, 'Ein weiterer Kommentar', '22:03:29')"
+sql = "INSERT INTO comments(id_Document, id_User, comment, datum) VALUES(3, 2, 'Ein weiterer Kommentar', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
-sql = "INSERT INTO comments(id_Document, id_User, id_Comment, comment, datum) VALUES(2, 3, 2, 'Ein rekursiver Kommentar', '22:03:29')"
+sql = "INSERT INTO comments(id_Document, id_User, id_Comment, comment, datum) VALUES(2, 3, 2, 'Ein rekursiver Kommentar', '1092941466')"
 cursor.execute(sql)
 connection.commit()
 
@@ -109,3 +109,5 @@ cursor.execute(sql)
 connection.commit()
 
 connection.close()
+
+#Datum Unix Timestamp format
