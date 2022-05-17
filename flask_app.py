8cf116c880d5b003fea4be51dfcbbd72fe0e18e6
lorinloewe4444 <65305474+lorinloewe4444@users.noreply.github.com>
2022-05-17 15:51:21 +0200

    jajaja

--- flask_app.py
+++ flask_app.py
@@ -13,7 +13,7 @@ my_form="""
 
             <h1>Formular f&uuml;r Namenseingabe</h1>
 
-              <form action="setcookie" method="post">
+              <form action="bimformgsi" method="post">
                 <p>Vorname:<br><input name="vorname" type="text" size="30" maxlength="3000"></p>
                 <p>Zuname:<br><input name="name" type="text" size="30" maxlength="40"></p>
                 <p><input type="submit" value=" Absenden "> <input type="reset" value=" Abbrechen"></p>
@@ -29,12 +29,18 @@ def anmelden():
     return resp
 
 @app.route('/form')
-def form():
-    return my_form
+def form():        
+  return my_form
 
+@app.route('/bimformgsi')
+def bimformgsi():
+  if request.method == 'POST':
+        resp = make_response("<script>window.location.href='/setcookie'</script>")
+        resp.set_cookie('legalunterwaegs',expires=datetime.datetime.now() + datetime.timedelta(seconds=10))
+        return resp
 @app.route('/setcookie', methods=['POST', 'GET'])
 def setcookie():
-  if "Name" in request.cookies:
+  if "legalunterwaegs" in request.cookies:
      if request.method == 'POST':
         name_cookie=request.form['vorname']+"  "+request.form['name']
         resp = make_response("Erfolgreich angemeldet. Hier kannst du deine Angaben überprüfen oder dich abmelden:<br><a href='/getcookie'>Überprüfen&Abmelden</a><br>")
@@ -49,13 +55,22 @@ def setcookie():
 
 @app.route('/getcookie')
 def getcookie():
+  if "Name" in request.cookies:
     name = request.cookies.get('Name')
     return "Name : "+name+ "<br><a href='/delcookie '>ABMELDEN</a><br>"
+  else:
+    resp= make_response('<script>window. location. href="/";</script>')
+    return resp
+    
 
 @app.route('/delcookie')
 def delcookie():
+  if "Name" in request.cookies:
     resp=make_response("erfolgreich abgemeldet")
     resp.delete_cookie('Name')
+    return resp
+  else:
+    resp= make_response('<script>window. location. href="/";</script>')
     return resp
 
 if __name__ == '__main__':
