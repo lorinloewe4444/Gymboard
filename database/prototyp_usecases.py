9385ff35fa70b415109b8fc1963338c9fe1ee4d5
macello <96392254+macello@users.noreply.github.com>
2022-05-31 16:24:30 +0200

    Update prototyp_usecases.py

--- database/prototyp_usecases.py
+++ database/prototyp_usecases.py
@@ -9,8 +9,18 @@ def tags(parent_id):
     con = connection()
     cur = con.cursor()
     tags = []
-    cur.execute("SELECT name FROM tags WHERE parent_id = %d" % (parent_id))
+    cur.execute("SELECT id, name FROM tags WHERE parent_id = %d;" % (parent_id))
     for dsatz in cur:
         tags.append(list(dsatz))
     con.close()
     return tags
+
+def documents_light(tags_id):
+    con = connection()
+    cur = con.cursor()
+    documents = []
+    cur.execute("SELECT id, name FROM documents WHERE tags_id = %d;" % (tags_id))
+    for dsatz in cur:
+        documents.append(list(dsatz))
+    con.close()
+    return documents
