71a96eb270ffe0c97193cc81b732d213d5cac242
macello <96392254+macello@users.noreply.github.com>
2022-05-31 16:00:23 +0200

    Update prototyp_usecases.py

--- database/prototyp_usecases.py
+++ database/prototyp_usecases.py
@@ -5,12 +5,14 @@ def connection():
     return sqlite3.connect('Gymboard.db')
 
 
-def tags(level):
+def tags(parent_id):
     con = connection()
     cur = con.cursor()
     tags = []
-    cur.execute("SELECT name FROM tags WHERE level = %d" % (level))
+    cur.execute("SELECT name FROM tags WHERE parent_id = %d" % (parent_id))
     for dsatz in cur:
         tags.append(list(dsatz))
     con.close()
     return tags
+
+print(tags(int(input())))
