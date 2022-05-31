9c97a04f55eb3926401879f397d4f59e6e64266a
macello <96392254+macello@users.noreply.github.com>
2022-05-31 16:36:43 +0200

    Update prototyp_usecases.py

--- database/prototyp_usecases.py
+++ database/prototyp_usecases.py
@@ -5,22 +5,34 @@ def connection():
     return sqlite3.connect('Gymboard.db')
 
 
-def tags(parent_id):
+def tags(id_Parent):
     con = connection()
     cur = con.cursor()
     tags = []
-    cur.execute("SELECT id, name FROM tags WHERE parent_id = %d;" % (parent_id))
+    cur.execute("SELECT id, name FROM tags WHERE id_Parent = %d;" % (id_Parent))
     for dsatz in cur:
         tags.append(list(dsatz))
     con.close()
     return tags
 
-def documents_light(tags_id):
+def documents_light(id_Tags):
     con = connection()
     cur = con.cursor()
     documents = []
-    cur.execute("SELECT id, name FROM documents WHERE tags_id = %d;" % (tags_id))
+    cur.execute("SELECT id, name FROM documents WHERE id_Tags = %d;" % (id_Tags))
     for dsatz in cur:
         documents.append(list(dsatz))
     con.close()
     return documents
+
+def documents(id_Tags):
+    con = connection()
+    cur = con.cursor()
+    documents = []
+    cur.execute("SELECT documents.*, users.nickName FROM documents JOIN users ON documents.id_User=users.id WHERE id_Tags = %d;" % (id_Tags))
+    for dsatz in cur:
+        documents.append(list(dsatz))
+    con.close()
+    return documents
+
+print(documents(int(input())))
