ee2b4da6feb356b28c2f4b9be2554ff49a2ebf4a
DiaCreep <102427468+DiaCreep@users.noreply.github.com>
2022-06-14 16:20:26 +0200

    kommunikation zwischen Phyton und Javascript

--- root/home.py
+++ root/home.py
@@ -1,10 +1,24 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, jsonify, request, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
    return render_template("index.html")
+
+
+@app.route('/test<int:id>', methods=['GET', 'POST'])
+def testfn(id):
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        print(id)
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
